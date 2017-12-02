c7b671463001449782aba05836327b357faaad95
Dongle(Dongkyun Kim) <dgkim11@coupang.com>
2017-12-02 09:33:46 +0900

    setup the project

---
+++ members-domains/src/main/java/com/coupang/sharing/member/domain/Member.java
@@ -0,0 +1,18 @@
+package com.coupang.sharing.member.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * @author Dennis Kim
+ */
+@Getter
+@Builder
+public class Member {
+	private String firstName;
+	private String lastName;
+	private String userId;
+	private String password;
+	private String email;
+	private String phoneNumber;
+}
